6cd8dc95f402b33e84b666cf917aaf84c2313ba6
Mamedbekov Ruslan <mamedbekovruslan@yandex.ru>
2024-10-19 19:12:01 +0300

    First route and controller

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use("/api/users", userRoutes);
+
 app.get("/", (req, res) => res.send(`Server started on port ${PORT}`));
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
